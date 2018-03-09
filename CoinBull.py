cf9b4940fc80700de8bad8fa337542d7cb953714
Pablo Patino <patinopablo94@gmail.com>
2018-03-08 20:13:53 -0500

    Added a basic template that I can start working with.

--- CoinBull.py
+++ CoinBull.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/sorry')
+def page_not_here_yet():
+	return 'Sorry I have not made this page yet!'
 
 @app.route('/user/<username>')
 def get_username(username):

8927a108fca5e70d9e1bbbfd42efaaf579821a1f
Pablo Patino <patinopablo94@gmail.com>
2018-03-20 07:55:51 -0400

    Added some comments

--- CoinBull.py
+++ CoinBull.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
 import gdax
-import sqlalchemy
+
 app = Flask(__name__)
 
+## Configuration for what crypto currencies to track ##
 cryptos = {
 	'Bitcoin': {
 		'ticker': 'BTC',
@@ -22,31 +24,39 @@ cryptos = {
 		},
 	}
 
+## public client to handle connections to GDAX ##
 public_client = gdax.PublicClient()
 
 class Currency(object):
+	"""
+	This class holds all crypto currency information
+	"""
 	def __init__(self, name, ticker, picture_url):
 		self.name = name
 		self.ticker = ticker
 		self.usd_product = "{}-USD".format(self.ticker)
 		self.picture_url = picture_url
 
+	## Function to pull current price of a specified crypto currency ##
 	def get_price(self):
 		price_info = public_client.get_product_ticker(product_id=self.usd_product)
 		price_only = price_info.get('price')
 		price_two_decimal = price_only[:-6]
 		self.price = price_two_decimal
 
+	## Function to pull 24 hour statistics for a crypto currency ##
 	def get_42hr_stats(self):
 		stats_24_hour = public_client.get_product_24hr_stats(self.usd_product)
 		self.volume_24hr = stats_24_hour.get("volume")[:-6]
 		self.high_24hr = stats_24_hour.get("high")[:-6]
 		self.low_24hr = stats_24_hour.get("low")[:-6]
 
+	## Function to get the URL to GDAX for the current crypto currency object ##
 	def get_url(self):
 		url = "https://www.gdax.com/trade/{0}".format(self.usd_product)
 		self.url = url
 
+## Home page that displays basic price information ##
 @app.route('/')
 def index():
 	crypto_objects = []
--- tabledef.py
+++ tabledef.py
@@ -4,12 +4,17 @@ from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 
+## Initiate SQLite as the database engine and file location ##
 engine = create_engine('sqlite:///CoinBull.db', echo=True)
 Base = declarative_base()
 
 class User(Base):
+	"""
+	Define user class with basic username and password
+	"""
 	__tablename__ = "users"
 
+	## Define the data type for columns id, username, password ##
 	id = Column(Integer, primary_key=True)
 	username = Column(String)
 	password = Column(String)
@@ -18,4 +23,5 @@ class User(Base):
 		self.username = username
 		self.password = password
 
+## Publish changes to database ##
 Base.metadata.create_all(engine)

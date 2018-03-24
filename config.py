3142ebd544a477150e8611760aa3e0b9e53765a0
Pablo Patino <patinopablo94@gmail.com>
2018-03-24 14:40:53 -0400

    Added a 404 page

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# Enabling development environment
+# Enabling development environmentr
 DEBUG = True
 
 # Define the application directory
@@ -9,6 +9,8 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite://' + os.path.join(BASE_DIR, 'CoinBull.py')
 DATABASE_CONNECT_OPTIONS = {}
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
 CSRF_ENABLED     = True
 
@@ -17,4 +19,4 @@ CSRF_ENABLED     = True
 CSRF_SESSION_KEY = "secret"
 
 # Secret key for signing cookies
-SECRET_KEY = "secret"
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
